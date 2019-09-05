53daece1729095812c1a9166beab63a74f73ea7a
sriley <sarahriley.dev@gmail.com>
2019-09-05 18:48:44 -0400

    add header/nav

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,23 +1,24 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import StreamCreate from './streams/StreamCreate';
-import StreamList from './streams/StreamList';
-import StreamShow from './streams/StreamShow';
-import StreamEdit from './streams/StreamEdit';
-import StreamDelete from './streams/StreamDelete';
-
+import StreamCreate from "./streams/StreamCreate";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={StreamList}/>
+          <Header />
+          <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/edit" exact component={StreamEdit}/>
-          <Route path="/streams/delete" exact component={StreamDelete}/>
-          <Route path="/streams/show" exact component={StreamShow}/>
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
