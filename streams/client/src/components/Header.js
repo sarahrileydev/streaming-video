dcdd1b8a28f78fec85bf4be9995e6fd177a2bbd3
sriley <sarahriley.dev@gmail.com>
2019-09-08 12:41:39 -0400

    fix typo

--- streams/client/src/components/Header.js
+++ streams/client/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import GoogleAuth from "./GoggleAuth";
+import GoogleAuth from "./GoogleAuth";
 
 const Header = () => {
   return (
