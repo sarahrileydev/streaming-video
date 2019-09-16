aa1fc120472e600221ddf886e0c3de425fae8f57
sriley <sarahriley.dev@gmail.com>
2019-09-16 11:13:06 -0400

    action creator for creating stream

--- streams/client/src/actions/types.js
+++ streams/client/src/actions/types.js
@@ -1,4 +1,5 @@
 // store strings as variables to get an error message if there is a typo
 
-export const SIGN_IN = 'SIGN_IN';
-export const SIGN_OUT = 'SIGN_OUT';
+export const SIGN_IN = "SIGN_IN";
+export const SIGN_OUT = "SIGN_OUT";
+export const CREATE_STREAM = "CREATE_STREAM";
