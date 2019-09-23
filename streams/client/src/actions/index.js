102fd3c63bfc8f2a536951895a23d33065d5013e
sriley <sarahriley.dev@gmail.com>
2019-09-23 10:14:16 -0400

    add userID to streams

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -22,8 +22,9 @@ export const signOut = () => {
   };
 };
 
-export const createStream = formValues => async dispatch => {
-  const response = await streams.post("/streams", formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post("/streams", { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
@@ -46,5 +47,5 @@ export const editStream = (id, formValues) => async dispatch => {
 export const deleteStream = id => async dispatch => {
   await streams.delete(`/streams/${id}`);
 
-  dispatch({type: DELETE_STREAM, payload: id})
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
