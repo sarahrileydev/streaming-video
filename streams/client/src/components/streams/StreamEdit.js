519393e07f7e0d3bc8f4e82ab408982c0fcc700d
sriley <sarahriley.dev@gmail.com>
2019-10-03 19:38:35 -0400

    selecting records

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { connect } from 'react-redux'
 
-const StreamEdit = () => {
+const StreamEdit = props => {
   return <div>StreamEdit</div>;
 };
 
-export default StreamEdit;
+// any time we call mapStateToProps we have state available, as well as props available from the component we are in
+mapStateToProps = (state, ownProps) => {
+  return {stream: state.streams[ownProps.match.params.id]}
+}
+
+export default connect(mapStateToProps)(StreamEdit);
