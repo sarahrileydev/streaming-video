a56b560119220f1c128ff0386fc7f21529488d57
sriley <sarahriley.dev@gmail.com>
2019-10-05 17:01:10 -0400

    add stream edit component

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,13 +1,24 @@
 import React from "react";
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamEdit = props => {
-  return <div>StreamEdit</div>;
-};
-
-// any time we call mapStateToProps we have state available, as well as props available from the component we are in
-mapStateToProps = (state, ownProps) => {
-  return {stream: state.streams[ownProps.match.params.id]}
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return <div>{this.props.stream.title}</div>;
+  }
 }
 
-export default connect(mapStateToProps)(StreamEdit);
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamEdit);
